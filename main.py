bfea62a59254a69f591f554ab24b7af3ef99d194
Chris-Chen <61469920+chrischen842@users.noreply.github.com>
2021-12-25 01:19:23 +0000

    added !new and !del

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 import json
 import random
+from replit import db
 
 client = discord.Client()
 
@@ -22,23 +23,55 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return (quote)
 
+def update_encourage(encourage_msg):
+  if "encouragements" in db.keys():
+    encouragements = db['encouragements']
+    encouragements.append(encourage_msg)
+    db['encouragements'] = encouragements
+  else:
+    db['encouragements'] = [encourage_msg]
+
+def remove_encourage(index):
+  encouragements = db['encouragements']
+  if len(encouragements) > index:
+    del encouragements[index]
+    db['encouragements'] = encouragements
+
 @client.event
-async def on_ready() :
+async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+
+  msg = message.content 
+
   if message.author == client.user:
     return
 
-  if message.content.startswith('!quote'):
+  if msg.startswith('!quote'):
     quote = get_quote()
     await message.channel.send(quote)
 
-  if message.content.startswith('!hello'):
-    await message.channel.send('Hello there!')
+  options = encourage
+  if 'encouragements' in db.keys():
+    options = options + list(db['encouragements'])
 
-  if any(word in message.content for word in sad):
-    await message.channel.send(random.choice(encourage))
+  if any(word in msg for word in sad):
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith('!new'):
+    encourage_msg = msg.split('!new ',1)[1]
+    update_encourage(encourage_msg)
+    await message.channel.send('New encouragement added!')
+    
+  
+  if msg.startswith('!del'):
+    encouragements = []
+    if 'encouragements' in db.keys():
+      index = int(msg.split('!del',1)[1])
+      remove_encourage(index)
+      encouragements = db['encouragements']
+    await message.channel.send(encouragements)
 
 client.run(os.getenv('TOKEN'))
